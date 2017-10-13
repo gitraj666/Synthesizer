08ab0c7b7a0c0999720ff3b2fd618866c5a4c60e
ramraj666 <pmaurya196@gmail.com>
2017-10-14 00:53:23 +0530

    keyboard is working now

--- keyboard_gui.py
+++ keyboard_gui.py
@@ -4,7 +4,9 @@ import pyaudio
 import tkinter as tk
 import numpy as np
 import matplotlib.pyplot as plt
+from tkinter import *
 root = tk.Tk()
+root.state('zoomed')
 
 
 #Variables :
@@ -60,7 +62,6 @@ def plot_square(freq,amplitude,phase):
     ymax=2+amplitude
     offset=1
     x=np.sort(np.concatenate([np.arange(xmin, xmax)-1E-6,np.arange(xmin, xmax)+1E-6]))
-    #You can use np.linspace(xmin,xmax,Nx) if you want the intermediate points
 
     y=((np.array(x+n+offset,dtype=int)%2)*amplitude)
 
@@ -168,6 +169,50 @@ def plot_osc1():
 def plot_osc2():
     pass
 
+def keys():
+        white_key_width = 30
+        white_key_height = 110
+        black_key_width = white_key_width * 0.5
+        black_key_height = white_key_height * 0.6
+        num_octaves = 5
+        first_octave = 2
+        x_offset = 3
+        y_offset = 20
+        canvas = tk.Canvas( width=white_key_width*num_octaves*7+2, height=white_key_height+10+y_offset, borderwidth=0)
+        # white keys:
+        for key_nr, key in enumerate("CDEFGAB"*num_octaves):
+            octave = first_octave+key_nr//7
+            def key_pressed(event, note=key, octave=octave):
+                force = min(white_key_height, event.y*1.08)/white_key_height   # @todo control output volume, unused for now...
+                play_osc1()
+            def key_released(event, note=key, octave=octave):
+                pass
+            x = key_nr * white_key_width
+            key_rect = canvas.create_rectangle(x+x_offset, y_offset, x+white_key_width+x_offset, white_key_height+y_offset, fill="white", outline="gray50", width=1, activewidth=2)
+            canvas.tag_bind(key_rect, "<ButtonPress-1>", key_pressed)
+            canvas.tag_bind(key_rect, "<ButtonRelease-1>", key_released)
+            canvas.create_text(x+white_key_width/2+2, 1, text=key, anchor=tk.N, fill="gray")
+            if 10 <= key_nr <= 21:
+                keychar = "qwertyuiop[]"[key_nr-10]
+                canvas.create_text(x+white_key_width/2+2, white_key_height+3, text=keychar, anchor=tk.N, fill="maroon")
+                pass
+        # black keys:
+        for key_nr, key in enumerate((["C#", "D#", None, "F#", "G#", "A#", None]*num_octaves)[:-1]):
+            if key:
+                octave = first_octave + key_nr // 7
+                def key_pressed(event, note=key, octave=octave):
+                    force = min(black_key_height, event.y * 1.1) / black_key_height   # @todo control output volume, unused for now...
+                    pass
+                def key_released(event, note=key, octave=octave):
+                    pass
+                x = key_nr * white_key_width + white_key_width*0.75
+                key_rect = canvas.create_rectangle(x+x_offset, y_offset, x+black_key_width+x_offset, black_key_height+y_offset, fill="black", outline="gray50", width=1, activewidth=2)
+                canvas.tag_bind(key_rect, "<ButtonPress-1>", key_pressed)
+                canvas.tag_bind(key_rect, "<ButtonRelease-1>", key_released)
+        canvas.pack(padx=10,side="bottom")
+
+
+
 #Oscillator 1:
 row = 0
 waveforms = ["sine", "triangle", "pulse", "sawtooth", "square"]
@@ -220,36 +265,12 @@ button_plotosc2.grid(row=row,column=2)
 button_plotosc2 = tk.Button(f2,text="PLAY",command=play_osc2)
 button_plotosc2.grid(row=row,column=3)
 
-def keys():
-    white_key_width = 30
-    white_key_height = 110
-    black_key_width = white_key_width * 0.5
-    black_key_height = white_key_height * 0.6
-    num_octaves = 5
-    first_octave = 2
-    x_offset = 3
-    y_offset = 20
-    canvas = tk.Canvas(width=white_key_width*num_octaves*7+2, height=white_key_height+10+y_offset, borderwidth=0)
-    # white keys:
-    for key_nr, key in enumerate("CDEFGAB"*num_octaves):
-        octave = first_octave+key_nr//7
-        x = key_nr * white_key_width
-        key_rect = canvas.create_rectangle(x+x_offset, y_offset, x+white_key_width+x_offset, white_key_height+y_offset, fill="white", outline="gray50", width=1, activewidth=2)
-        canvas.create_text(x+white_key_width/2+2, 1, text=key, anchor=tk.N, fill="gray")
 
 
-    # black keys:
-    for key_nr, key in enumerate((["C#", "D#", None, "F#", "G#", "A#", None]*num_octaves)[:-1]):
-        if key:
-            octave = first_octave + key_nr // 7
-            x = key_nr * white_key_width + white_key_width*0.75
-            key_rect = canvas.create_rectangle(x+x_offset, y_offset, x+black_key_width+x_offset, black_key_height+y_offset, fill="black", outline="gray50", width=1, activewidth=2)
-    canvas.pack(side=tk.BOTTOM)
-
-
-f1.pack(side=tk.LEFT)
-f2.pack(side=tk.RIGHT)
 keys()
+f1.pack(side=tk.LEFT)
+f2.pack(padx=15,side=tk.LEFT)
+
 root.mainloop()
 
 
